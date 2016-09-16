50a3395aeab823fc4791c7e51637976f77610ba0
Sebastian Guillema <sguillema@hotmail.com>
2016-09-16 16:21:47 +1000

    Team Menu layout finalised

--- client/user/manage-teams/components/TeamMenu.js
+++ client/user/manage-teams/components/TeamMenu.js
@@ -9,21 +9,42 @@ export default class TeamMenu extends React.Component{
         super()
     }
 
+    didComponentMount(){
+        $('.menu .item').tab();
+    }
+
     render(){
-        //todo: make it a split menu
-        //ui two wide column secondary vertical pointing menu
         return(
-            <div className="ui two column row">
-                <div className="column">
-                    <a className="item active">
-                        Overview
-                    </a>
-                    <a className="item">{/*there needs to be a condition here that prevents applications from showing if they aren't a team leader*/}
-                        Applications
-                    </a>
+            <div className="ui stretched row two column grid">
+                <div className="ui top attached blue segment row centered">
+                    <h1 className="header column">Team Name</h1>
                 </div>
-                <div className="column">
-                    Example Text. YOLO!
+                <div className="ui attached segment row">
+                    <div className="two wide column">
+                        <div className="ui vertical tabular menu">
+                            <a className="active blue item" data-tab="first">
+                                Overview
+                            </a>
+                            <a className="blue item" data-tab="second">
+                                Applications
+                            </a>
+                        </div>
+                    </div>
+                    <div className="right floated twelve wide column">
+                        <div className="ui active tab stretched" data-tab="first">
+                            <h2 className="ui top attached blue segment header">Overview</h2>
+                            <div className="ui attached segment">
+                                <p>Leaders: Player 1</p>
+                                <p>Members: Players 1, 2, 3, 4, and 5</p>
+                            </div>
+                        </div>
+                        <div className="ui tab stretched" data-tab="second">
+                            <h2 className="ui top attached blue segment header">Applications</h2>
+                            <div className="ui attached segment">
+                                <p>Hello 2</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
