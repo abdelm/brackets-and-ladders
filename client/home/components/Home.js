2de652a9949ab419f46dfd3e565af19ffd1e9d1d
Abdelrahman Ahmed <abdelm.is@gmail.com>
2016-09-19 13:26:09 +1000

    Hid the sign up button for logged in users

--- client/home/components/Home.js
+++ client/home/components/Home.js
@@ -27,13 +27,20 @@ export default class Home extends React.Component{
             letterSpacing: '1px',
         }
 
+        let homeButtons = null;
+        // Hide sign up if user is logged in
+        // @todo new buttons for logged in members?
+        if (!Meteor.userId()){
+            homeButtons = <a className="ui inverted large button" href="/register">SIGN UP</a>;
+        }
+
         return (
             <div style={mastheadStyle} className="ui centered padded grid">
                 <div className="column middle aligned ui centered grid">
                     <h1 style={headerStyle} className="ui column middle aligned header inverted row">Brackets and Ladders</h1>
                     <h2 style={subHeaderStyle} className="ui column middle aligned header inverted row">Your place for community driven tournaments</h2>
                     <div className="ui column middle aligned header row">
-                        <a className="ui inverted large button" href="/register">SIGN UP</a>
+                        {homeButtons}
                     </div>
                 </div>
             </div>
